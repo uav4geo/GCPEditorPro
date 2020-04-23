7a0e99ebc65e48fcb40b0ad7e5f57d8e570f534d
Piero Toffanin <pt@masseranolabs.com>
2020-04-23 19:19:38 -0400

    WebODM plugin working

--- dist.sh
+++ dist.sh
@@ -7,7 +7,14 @@ npm run dist
 cd dist
 if [ -e ./GCPEditorPro.AppImage ]; then
 	# Linux
-	mv GCPEditorPro.AppImage GCPEditorPro
+	mv -v GCPEditorPro.AppImage GCPEditorPro
 	zip GCPEditorPro-Linux.zip GCPEditorPro
+
+    # Make WebODM plugin
+    mkdir -p -v plugin-staging/
+    cp -R ../plugin ./plugin-staging/gcp-editor-pro
+    cp -R gcp-editor-pro ./plugin-staging/gcp-editor-pro/public
+    cd plugin-staging
+    zip -r ../GCPEditorPro-WebODM-Plugin.zip gcp-editor-pro
 fi
 

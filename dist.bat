a82fc1759700e9252d958ad6d44e23085d31e0dd
Piero Toffanin <pt@masseranolabs.com>
2020-04-23 19:36:58 -0400

    Yarn

--- dist.bat
+++ dist.bat
@@ -1,2 +1,7 @@
-npm run electron-build
-npm run dist
+yarn
+rd /s /q dist
+call npm run electron-build
+call npm run dist
+
+cd dist
+powershell Compress-Archive GCPEditorPro.exe GCPEditorPro-Windows.zip

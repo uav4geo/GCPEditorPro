0c84d2bbe44309b8f79ba70e4484c0737b58fcaf
Joe <52297861+JoeKae@users.noreply.github.com>
2024-01-11 13:31:58 +0100

    Adds option to replace NaN with 0 on Z column

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { LoadConfigComponent } from './load-config/load-config.component';
-import { ExportConfigComponent } from './export-config/export-config.component';
-import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {LoadConfigComponent} from './load-config/load-config.component';
+import {ExportConfigComponent} from './export-config/export-config.component';
+import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {HashLocationStrategy, LocationStrategy} from '@angular/common';
 
-import { FormsModule } from '@angular/forms';
-import { LoadConfigCsvComponent } from './load-config-csv/load-config-csv.component';
-import { LoadConfigTxtComponent } from './load-config-txt/load-config-txt.component';
-import { LoadConfigEmptyComponent } from './load-config-empty/load-config-empty.component';
-import { LicenseInfoComponent } from './license-info/license-info.component';
-import { GcpsMapComponent } from './gcps-map/gcps-map.component';
-import { ImagesTaggerComponent } from './images-tagger/images-tagger.component';
-import { LeafletModule } from '@asymmetrik/ngx-leaflet';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
-import { SmartimageComponent } from './smartimage/smartimage.component';
-import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
-import { NgOpenCVModule, OpenCVOptions } from "ng-open-cv";
+import {FormsModule} from '@angular/forms';
+import {LoadConfigCsvComponent} from './load-config-csv/load-config-csv.component';
+import {LoadConfigTxtComponent} from './load-config-txt/load-config-txt.component';
+import {LoadConfigEmptyComponent} from './load-config-empty/load-config-empty.component';
+import {LicenseInfoComponent} from './license-info/license-info.component';
+import {GcpsMapComponent} from './gcps-map/gcps-map.component';
+import {ImagesTaggerComponent} from './images-tagger/images-tagger.component';
+import {LeafletModule} from '@asymmetrik/ngx-leaflet';
+import {fas} from '@fortawesome/free-solid-svg-icons';
+import {far} from '@fortawesome/free-regular-svg-icons';
+import {SmartimageComponent} from './smartimage/smartimage.component';
+import {ConfirmDialogComponent} from './confirm-dialog/confirm-dialog.component';
+import {NgOpenCVModule, OpenCVOptions} from 'ng-open-cv';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const openCVConfig: OpenCVOptions = {
     scriptUrl: `assets/opencv/opencv.js`,
@@ -51,6 +53,8 @@ const openCVConfig: OpenCVOptions = {
         FormsModule,
         LeafletModule.forRoot(),
         NgOpenCVModule.forRoot(openCVConfig),
+        MatSlideToggleModule,
+        BrowserAnimationsModule
     ],
     providers: [
         {

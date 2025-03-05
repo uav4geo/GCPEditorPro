12b2be5e435cd2f6f1207ec48596e171748597fc
Piero Toffanin <pt@masseranolabs.com>
2025-03-04 23:43:16 -0500

    Fix Windows dist

--- dist.bat
+++ dist.bat
@@ -1,7 +1,8 @@
 call yarn
 rd /s /q dist
+set NODE_OPTIONS=--openssl-legacy-provider
 call npm run electron-build
-call npm run dist
+.\node_modules\.bin\electron-builder
 
 cd dist
 powershell Compress-Archive GCPEditorPro.exe GCPEditorPro-Windows.zip

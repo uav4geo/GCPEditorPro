20fb1dd189f9e36f53ec047eeb8842fae0755dd2
Piero Toffanin <pt@masseranolabs.com>
2025-10-15 16:18:28 -0400

    Fix build

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -61,7 +61,6 @@ import 'classlist.js';  // Run `npm install --save classlist.js`.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
-
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
